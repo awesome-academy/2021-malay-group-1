a562dd0330c66ca9287e37dab25016b41e1c6125
daniadore <daniadore13@gmail.com>
2021-09-22 13:11:47 +0800

    Account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
@@ -57,9 +58,18 @@ end
     reset_sent_at < 2.hours.ago
   end
 
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
   private
 
   def downcase_email
     self.email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
